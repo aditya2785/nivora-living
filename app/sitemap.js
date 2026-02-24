bd5d1cc204f5c2dd7552df96b327a34ead39f944
Aditya <24f2005506@ds.study.iitm.ac.in>
2026-02-24 19:06:49 +0530

    Fixed

---
+++ app/sitemap.js
@@ -0,0 +1,20 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://nivoraliving.co",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://nivoraliving.co/shop",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://nivoraliving.co/about",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://nivoraliving.co/contact",
+      lastModified: new Date(),
+    },
+  ];
+}
